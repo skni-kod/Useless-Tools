2bd7001f1f76ff0eaf6cf805173329c85463a7d9
PawelOchal <opawel262@gmail.com>
2022-12-24 01:49:55 +0100

    url routing

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from random import choice
 
-# Create your views here.
+def home(request):
+  return render(request,'core/home.html')
+
+
+def generator(request):
+    return render(request,'core/generator.html')
+
+    
+def password(request):
+    if request.method == 'POST':
+        length = int(request.POST.get('length'))
+        uppercase = request.POST.get('uppercase')
+        symbols = request.POST.get('symbols')
+        numbers = request.POST.get('numbers')
+        chars = list('qwertyuiopasdfghjklzxcvbnm')
+        if(uppercase):
+            chars.extend('QWERTYUIOPASDFGHJKLZXCVBNM')
+        if(symbols):
+            chars.extend('!@#$%^&*')
+        if(numbers):
+            chars.extend('123456789')
+        generated_PASS = ''
+        for x in range(length):
+            generated_PASS += choice(chars)
+        return render(request,'core/generator.html',{'password':generated_PASS})
