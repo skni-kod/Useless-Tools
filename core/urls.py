b6cef0adb2be74627114f35f07c59d82503c9a67
PawelOchal <opawel262@gmail.com>
2023-02-17 23:20:56 +0100

    authentication and some other small adjustments

--- core/urls.py
+++ core/urls.py
@@ -3,16 +3,15 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
-
-
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('generator',views.generator,name='generator'),
-    path('generate-password',views.password, name = 'password'),
-    path('signup',views.signup, name = 'signup'),
-    path('signin',views.signin, name = 'signin'),
-    
-    
+    path('', views.home, name='home'),
+    path('generator', views.generator, name='generator'),
+    path('generate-password', views.password, name='password'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+
+
 
 ]
 urlpatterns += staticfiles_urlpatterns()
--- core/views.py
+++ core/views.py
@@ -5,13 +5,16 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
-  return render(request,'core/home.html')
+    return render(request, 'core/home.html')
 
 
 def generator(request):
-    return render(request,'core/generator.html')
+    return render(request, 'core/generator.html')
+
 
 def password(request):
     if request.method == 'POST':
@@ -21,16 +24,16 @@ def password(request):
         numbers = request.POST.get('numbers')
         lowercase = request.POST.get('lowercase')
         chars = []
-        if(lowercase):
+        if (lowercase):
             chars.extend(string.ascii_lowercase)
-        if(uppercase):
+        if (uppercase):
             chars.extend(string.ascii_uppercase)
-        if(symbols):
+        if (symbols):
             chars.extend('!@#$%^&*')
-        if(numbers):
+        if (numbers):
             chars.extend('1234567890')
         generated_PASS = ''
-        if(numbers or lowercase or symbols or uppercase):
+        if (numbers or lowercase or symbols or uppercase):
             for x in range(length):
                 generated_PASS += choice(chars)
         else:
@@ -42,21 +45,38 @@ def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
-        password = request.POST.get('password')
-        password2 = request.POST.get('confirm_password')
-        myuser = User.objects.create_user(username,email,password)
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        myuser = User.objects.create_user(username, email, pass1)
         myuser.save()
 
-        messages.success(request,'Twoje konto zostało utworzone!')
+        messages.success(request, 'Twoje konto zostało utworzone!')
 
         return redirect('signin')
-       
-    return render(request,'core/signup.html')
+
+    return render(request, 'core/signup.html')
+
 
 def signin(request):
-    return render(request,'core/signin.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Zalogowano pomyślnie!')
+            return render(request, 'core/home.html', {'user': user})
+        else:
+            messages.error(request, 'Błędny login lub hasło!')
+            return redirect('signin')
+
+    return render(request, 'core/signin.html')
+
 
 def signout(request):
-    pass
-
-
+    logout(request)
+    messages.success(request, 'Wylogowano pomyślnie!')
+    return redirect('home')
