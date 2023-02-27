682cedef36338b258993f4b30527329a2998c955
PawelOchal <opawel262@gmail.com>
2023-02-27 19:02:51 +0100

    added models for user and stats

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class stats(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stats")
+    score = models.IntegerField(default=0)
+    time = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.score} {self.time}"
