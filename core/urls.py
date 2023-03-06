3e37fb8c3844788cf717329ce1ee5674ee9c5d26
Kazan1520 <Kazan1520@users.noreply.github.com>
2023-03-06 20:32:31 +0000

    Format code with black

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,14 @@
-from django.urls import path
-from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 
+from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('generator',views.generator,name='generator'),
-    path('generate-password',views.password, name = 'password'),
-    path('signup',views.signup,name='signup'),
-    path('signin',views.signin,name='signin'),
-    path('signout',views.signout,name='signout')
-
-
+    path("", views.home, name="home"),
+    path("generator", views.generator, name="generator"),
+    path("generate-password", views.password, name="password"),
+    path("signup", views.signup, name="signup"),
+    path("signin", views.signin, name="signin"),
+    path("signout", views.signout, name="signout"),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- core/views.py
+++ core/views.py
@@ -1,83 +1,81 @@
-from django.shortcuts import render
 import string
 from random import choice
-from django.http import JsonResponse
-from django.contrib.auth.models import User
+
 from django.contrib import messages
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, "core/home.html")
 
 
 def generator(request):
-    return render(request, 'core/generator.html')
+    return render(request, "core/generator.html")
 
 
 def password(request):
-    if request.method == 'POST':
-        length = int(request.POST.get('length'))
-        uppercase = request.POST.get('uppercase')
-        symbols = request.POST.get('symbols')
-        numbers = request.POST.get('numbers')
-        lowercase = request.POST.get('lowercase')
+    if request.method == "POST":
+        length = int(request.POST.get("length"))
+        uppercase = request.POST.get("uppercase")
+        symbols = request.POST.get("symbols")
+        numbers = request.POST.get("numbers")
+        lowercase = request.POST.get("lowercase")
         chars = []
-        if (lowercase):
+        if lowercase:
             chars.extend(string.ascii_lowercase)
-        if (uppercase):
+        if uppercase:
             chars.extend(string.ascii_uppercase)
-        if (symbols):
-            chars.extend('!@#$%^&*')
-        if (numbers):
-            chars.extend('1234567890')
-        generated_PASS = ''
-        if (numbers or lowercase or symbols or uppercase):
+        if symbols:
+            chars.extend("!@#$%^&*")
+        if numbers:
+            chars.extend("1234567890")
+        generated_PASS = ""
+        if numbers or lowercase or symbols or uppercase:
             for x in range(length):
                 generated_PASS += choice(chars)
         else:
-            generated_PASS = 'zaznacz cos wrr'
-        return JsonResponse({'password': generated_PASS})
-  
-    
+            generated_PASS = "zaznacz cos wrr"
+        return JsonResponse({"password": generated_PASS})
+
+
 def signup(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        pass1 = request.POST.get('pass1')
-        pass2 = request.POST.get('pass2')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("pass1")
+        request.POST.get("pass2")
         myuser = User.objects.create_user(username, email, pass1)
         myuser.save()
 
-        messages.success(request, 'Twoje konto zostało utworzone!')
+        messages.success(request, "Twoje konto zostało utworzone!")
 
-        return redirect('signin')
+        return redirect("signin")
 
-    return render(request, 'core/signup.html')
+    return render(request, "core/signup.html")
 
 
 def signin(request):
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        pass1 = request.POST.get('pass1')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        pass1 = request.POST.get("pass1")
 
         user = authenticate(username=username, password=pass1)
 
         if user is not None:
             login(request, user)
-            messages.success(request, 'Zalogowano pomyślnie!')
-            return redirect('home')
+            messages.success(request, "Zalogowano pomyślnie!")
+            return redirect("home")
         else:
-            messages.error(request, 'Błędny login lub hasło!')
-            return redirect('signin')
+            messages.error(request, "Błędny login lub hasło!")
+            return redirect("signin")
 
-    return render(request, 'core/signin.html')
-
+    return render(request, "core/signin.html")
 
 
 def signout(request):
     logout(request)
-    messages.success(request, 'Wylogowano pomyślnie!')
-    return redirect('home')
+    messages.success(request, "Wylogowano pomyślnie!")
+    return redirect("home")
