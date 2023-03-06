70bd889794aca6612f10f66ec9fa48980b16056a
PawelOchal <opawel262@gmail.com>
2023-03-06 20:22:16 +0100

    added urls for login,logout and register pages

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
+from django.conf import settings
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('home',views.home,name='home'),
     path('generator',views.generator,name='generator'),
     path('generate-password',views.password, name = 'password'),
-    path('signup',views.signup,name='signup'),
-    path('signin',views.signin,name='signin'),
-    path('signout',views.signout,name='signout')
-
+    path('signup',views.signup.as_view(),name='signup'),
+    path('signin',LoginView.as_view(template_name='registration/signin.html'),name='signin'),
+    path('logout',LogoutView.as_view(),name='logout'),
+    
 
 ]
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
