08c1be3f293fc0c87a11405281c36d7ece895d10
Kazan1520 <Kazan1520@users.noreply.github.com>
2023-03-07 14:47:56 +0000

    Format code with black

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import reverse, render
 import string
 from random import choice
+
 from django.http import JsonResponse
-from .forms import CustomUserCreationForm
+from django.shortcuts import render, reverse
 from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
 
 
 def home(request):
@@ -15,12 +17,12 @@ def generator(request):
 
 
 def password(request):
-    if request.method == 'POST':
-        length = int(request.POST.get('length'))
-        uppercase = request.POST.get('uppercase')
-        symbols = request.POST.get('symbols')
-        numbers = request.POST.get('numbers')
-        lowercase = request.POST.get('lowercase') 
+    if request.method == "POST":
+        length = int(request.POST.get("length"))
+        uppercase = request.POST.get("uppercase")
+        symbols = request.POST.get("symbols")
+        numbers = request.POST.get("numbers")
+        lowercase = request.POST.get("lowercase")
         chars = []
         if lowercase:
             chars.extend(string.ascii_lowercase)
@@ -35,14 +37,13 @@ def password(request):
             for x in range(length):
                 generated_PASS += choice(chars)
         else:
-            generated_PASS = 'zaznacz cos wrr'
-        return JsonResponse({'password': generated_PASS})
+            generated_PASS = "zaznacz cos wrr"
+        return JsonResponse({"password": generated_PASS})
 
 
 class Signup(CreateView):
     form_class = CustomUserCreationForm
-    template_name = 'registration/signup.html'
+    template_name = "registration/signup.html"
 
     def get_success_url(self):
-        return reverse('signin')
-        
+        return reverse("signin")
