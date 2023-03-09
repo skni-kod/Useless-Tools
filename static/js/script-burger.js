d426c0def7a80bfbd8322d7570c9efe2bf2980f4
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2023-03-09 17:25:21 +0100

    burger fix v2.0

--- static/js/script-burger.js
+++ static/js/script-burger.js
@@ -1,19 +1,19 @@
-const button = document.querySelector(".burger-btn");
-const nav = document.querySelector(".nav-burger");
-let allNavItems = document.querySelectorAll(".dropdown-item");
-let itemButton = document.querySelectorAll(".button-burger");
-let menuItem = document.querySelectorAll(".burger-item");
-let dropdown = document.querySelectorAll(".dropdown");
+const button = document.querySelector('.burger-btn');
+const nav = document.querySelector('.nav-burger');
+let allNavItems = document.querySelectorAll('.dropdown-item');
+let itemButton = document.querySelectorAll('.button-burger');
+let menuItem = document.querySelectorAll('.burger-item');
+let dropdown = document.querySelectorAll('.dropdown');
 // let dropdown = document.querySelectorAll('.dropdown-burger')
 
 const navTransform = () => {
-	nav.classList.toggle("nav--active");
+	nav.classList.toggle('nav--active');
 
-	if (nav.classList.contains("nav--active")) {
+	if (nav.classList.contains('nav--active')) {
 		burgerRemoveFocus();
-		button.classList.add("burger-btn-click");
+		button.classList.add('burger-btn-click');
 	} else {
-		button.classList.remove("burger-btn-click");
+		button.classList.remove('burger-btn-click');
 	}
 
 	navAnimation();
@@ -23,42 +23,54 @@ const navAnimation = () => {
 	let delayTime = 0;
 
 	menuItem.forEach((navItem) => {
-		navItem.classList.toggle("nav-animation");
-		navItem.style.animationDelay = "." + delayTime + "s";
+		navItem.classList.toggle('nav-animation');
+		navItem.style.animationDelay = '.' + delayTime + 's';
 		delayTime++;
 	});
 };
 
 const burgerButtons = function () {
 	const dropdown = this.nextElementSibling;
+	// const title = this.previousElementSibling;
 
-	if (!dropdown.classList.contains("dropdown-focused")) {
+	if (!dropdown.classList.contains('dropdown-focused')) {
 		burgerRemoveFocus();
-		dropdown.classList.add("dropdown-focused");
+
+		dropdown.classList.add('dropdown-focused');
+		this.classList.add('orange');
+		this.parentElement.style.transform = 'translateX(13px)';
+		// console.log(title);
 	} else {
-		dropdown.classList.remove("dropdown-focused");
+		dropdown.classList.remove('dropdown-focused');
+		this.classList.remove('orange');
+		this.parentElement.style.transform = 'translateX(0px)';
 	}
 };
 
 const burgerRemoveFocus = () => {
+	itemButton.forEach((burger) => {
+		burger.classList.remove('orange');
+		burger.parentElement.style.transform = 'translateX(0px)';
+	});
+
 	dropdown.forEach((animation) => {
-		console.log("2");
-		animation.classList.remove("dropdown-focused");
+		console.log('2');
+		animation.classList.remove('dropdown-focused');
 	});
 };
 
-const removeAll = () => {
-	burgerRemoveFocus();
-	button.classList.remove("burger-btn-click");
-	nav.classList.remove("nav--active");
-};
+// const removeAll = () => {
+// 	burgerRemoveFocus();
+// 	button.classList.remove("burger-btn-click");
+// 	nav.classList.remove("nav--active");
+// };
 
-button.addEventListener("click", () => {
+button.addEventListener('click', () => {
 	navTransform();
 });
 
 itemButton.forEach((burger) => {
-	burger.addEventListener("click", burgerButtons);
+	burger.addEventListener('click', burgerButtons);
 });
 
 // document.addEventListener("click", function (event) {
