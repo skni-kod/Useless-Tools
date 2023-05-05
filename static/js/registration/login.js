052417ddbf762933b95913b597b344bd142e46c0
Lukasz <7lukasz8@gmail.com>
2023-05-05 16:06:15 +0200

    login page v1 changes required

---
+++ static/js/registration/login.js
@@ -0,0 +1,198 @@
+const inputs = document.querySelectorAll("input");
+const sparkles = document.querySelectorAll(".sparkle");
+const wheelOne = document.querySelector(".wheel-one");
+const wheelTwo = document.querySelector(".wheel-two");
+const wheelThree = document.querySelector(".wheel-three");
+const letters = document.querySelectorAll(".letters");
+const letterbT = document.querySelector(".letter-b-T");
+const letterbO = document.querySelector(".letter-b-O");
+const letterbO2 = document.querySelector(".letter-b-O2");
+const letterbL = document.querySelector(".letter-b-L");
+const letterbS = document.querySelector(".letter-b-S");
+const jumpingLetters = document.querySelectorAll(".let");
+let isBroken = false;
+
+
+const jumpingAnimation = () => {
+    for (let i = 0; i < jumpingLetters.length; i++) {
+        setTimeout(() => {
+            console.log(i);
+            if(isBroken) return;
+            jumpingLetters[i].classList.add("jump");
+        }, i * 100);
+    }
+    jumpingLetters.forEach(letter => {
+        letter.classList.remove("jump");
+    })
+}
+
+const jumpingAnimationReverse = () => {
+    for (let i = jumpingLetters.length - 1; i + 1 > 0; i--) {
+        setTimeout(() => {
+            if(isBroken) return;
+            jumpingLetters[i].classList.add("jump");
+        }, (jumpingLetters.length - i) * 100);
+    }
+    jumpingLetters.forEach(letter => {
+        letter.classList.remove("jump");
+    })
+}
+
+const fallingLetters = () => {
+    letterbT.classList.remove("back-Tb");
+    letterbO.classList.remove("back-Ob");
+    letterbO2.classList.remove("back-Ob2");
+    letterbL.classList.remove("back-Lb");
+    letterbS.classList.remove("back-Sb");
+
+    letterbT.classList.add("falling-tools");
+    letterbO.classList.add("falling-tools");
+    letterbO2.classList.add("falling-tools");
+    letterbL.classList.add("falling-tools");        
+    letterbS.classList.add("falling-S-b");
+    
+    sparkles.forEach(sparkle => {
+        sparkle.classList.add("display-block");
+    })
+
+    letters.forEach(letter => {
+        letter.classList.remove("letters-shadow");
+    })
+
+    setTimeout(() => {
+        letterbT.classList.remove("falling-tools");
+        letterbO.classList.remove("falling-tools");
+        letterbO2.classList.remove("falling-tools");
+        letterbL.classList.remove("falling-tools");
+
+        
+        letterbT.classList.add("falling-T-b");
+        letterbO.classList.add("falling-O-b");
+        letterbO2.classList.add("falling-O-b2");
+        letterbL.classList.add("falling-L-b");
+    }, 300);
+}
+
+const backLetters = () => {
+    letterbT.classList.remove("falling-T-b");
+    letterbO.classList.remove("falling-O-b");
+    letterbO2.classList.remove("falling-O-b2");
+    letterbL.classList.remove("falling-L-b");
+    letterbS.classList.remove("falling-S-b");
+
+    letterbT.classList.add("back-Tb");
+    letterbO.classList.add("back-Ob");
+    letterbO2.classList.add("back-Ob2");
+    letterbL.classList.add("back-Lb");
+    letterbS.classList.add("back-Sb");
+
+    sparkles.forEach(sparkle => {
+        sparkle.classList.remove("display-block");
+    })
+
+    letters.forEach(letter => {
+        letter.classList.add("letters-shadow");
+    })
+
+    setTimeout(() => {
+        letterbT.classList.remove("back-Tb");
+        letterbO.classList.remove("back-Ob");
+        letterbO2.classList.remove("back-Ob2");
+        letterbL.classList.remove("back-Lb");
+        letterbS.classList.remove("back-Sb");
+        isBroken = false;
+    }, 1000);
+}
+
+const backAnimation = () => {
+    backLetters();
+}
+
+const brokenAnimation = () => {
+    isBroken = true;
+    jumpingLetters.forEach(letter => {
+        letter.classList.remove("jump");
+    })
+    fallingLetters();
+}
+
+const inputAnimation = (input) => {
+    const top = input.nextElementSibling;
+    const side = top.nextElementSibling
+    const bottomOne = side.nextElementSibling;
+    const bottomTwo = bottomOne.nextElementSibling;
+    const label = input.previousElementSibling;
+    const icon = label.previousElementSibling;
+
+    brokenAnimation();
+    
+    label.classList.remove("white");
+    side.classList.remove('white-bgc');
+    top.classList.remove('white-bgc');
+    bottomTwo.classList.remove('white-bgc');
+
+
+    icon.style.borderRight = "none";
+    label.classList.add("active-label");
+    top.style.width = "10%";
+    side.classList.remove('white-bgc');
+    bottomOne.classList.add("bottom-active");
+    bottomTwo.classList.add("bottom-active");
+}
+
+const clearInputAnimation = (input) => {
+    const top = input.nextElementSibling;
+    const side = top.nextElementSibling
+    const bottomOne = side.nextElementSibling;
+    const bottomTwo = bottomOne.nextElementSibling;
+    const label = input.previousElementSibling;
+    const icon = label.previousElementSibling;
+
+    backAnimation();
+
+    if(input.value === "") {
+        setTimeout(() => {
+            side.classList.add('white-bgc');
+        }, 220);
+        icon.style.borderRight = "1px solid white";
+        label.classList.remove("active-label");
+        top.style.width = "0%";
+        bottomOne.classList.remove("bottom-active");
+        bottomTwo.classList.remove("bottom-active");
+    } else {
+        label.classList.add("white");
+        side.classList.add('white-bgc');
+        top.classList.add('white-bgc');
+        bottomTwo.classList.add('white-bgc');
+    }
+}   
+
+
+setInterval(() => {
+    wheelOne.classList.toggle("reverse-animation");
+    wheelTwo.classList.toggle("reverse-animation");
+    wheelThree.classList.toggle("reverse-animation");
+}, 10000);
+
+setTimeout(() => {
+    jumpingAnimation();
+    setInterval(() => {
+        if(!isBroken && wheelOne.classList.contains("reverse-animation")) {
+            jumpingAnimationReverse();
+        } else if(!isBroken && !wheelOne.classList.contains("reverse-animation")) {
+            jumpingAnimation();
+        }
+    }, 5000);
+}, 2500);
+
+inputs.forEach(input => {
+    input.addEventListener("focus", () => {
+        inputAnimation(input);
+    })
+    input.addEventListener("blur", () => {
+        clearInputAnimation(input);
+    })
+})
+
+
+    
