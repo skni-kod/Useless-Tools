a419af57878c3b9c31dd3a4000c1915f951fd2ed
opawel262 <opawel262@users.noreply.github.com>
2023-05-07 17:06:09 +0000

    Format code with black

--- siteproject/settings.py
+++ siteproject/settings.py
@@ -130,11 +130,11 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "signin"
 LOGIN_URL = "signin"
 
-#SMTP configuration
+# SMTP configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'uselesstoolssknikod@gmail.com'
-#EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = "uselesstoolssknikod@gmail.com"
+# EMAIL_HOST_PASSWORD = ''
