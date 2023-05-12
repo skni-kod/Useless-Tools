992c41a2f223245bf06f4c73d0eaffff8e15240f
PawelOchal <opawel262@gmail.com>
2023-05-12 19:54:13 +0200

    deleted .idea and changed debug

--- siteproject/settings.py
+++ siteproject/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEYS")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 
 DEPLOY_TYPE = os.environ.get("DEPLOY_TYPE", "dev")
